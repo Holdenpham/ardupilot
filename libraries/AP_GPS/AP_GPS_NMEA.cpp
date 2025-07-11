fb28412c1ce79430899e2ccec1111b4596550e98
Pham Tien Hung <phamhung279@gmail.com>
2025-07-11 17:56:15 +0900

    fix error GPS code 3

--- libraries/AP_GPS/AP_GPS_NMEA.cpp
+++ libraries/AP_GPS/AP_GPS_NMEA.cpp
@@ -782,6 +782,7 @@ void AP_GPS_NMEA::parse_versiona_field(uint16_t term_number, const char *term)
     switch (term_number) {
     case 10:
         strncpy(v.type, _term, sizeof(v.type)-1);
+        v.type[sizeof(v.type) - 1] = '\0';
         break;
     case 11:
         strncpy(v.version, _term, sizeof(v.version)-1);
